188f94616e26f6457774202acb0e1d8484c46763
Peter John <zhou_bingye@outlook.com>
2025-06-04 09:23:01 +0800

    Update quant_cfg.py

--- quant_cfg.py
+++ quant_cfg.py
@@ -8,6 +8,7 @@ def get_quant_config_slm(model):
     q8_64_config = BaseQuantizeConfig(nbits=8, group_size=64) 
     q8_32_config = BaseQuantizeConfig(nbits=8, group_size=32) 
     q4_128_config = BaseQuantizeConfig(nbits=4, group_size=128) 
+    q4_256_config = BaseQuantizeConfig(nbits=4, group_size=256) 
     q4_64_config = BaseQuantizeConfig(nbits=4, group_size=64) 
     q4_32_config = BaseQuantizeConfig(nbits=4, group_size=32) 
     q2_128_config = BaseQuantizeConfig(nbits=2, group_size=128) 
@@ -15,13 +16,13 @@ def get_quant_config_slm(model):
     q2_32_config = BaseQuantizeConfig(nbits=2, group_size=32) 
     
     for i in range(n_layers):
-        quant_config[f'model.layers.{i}.self_attn.q_proj'] = q4_128_config
-        quant_config[f'model.layers.{i}.self_attn.k_proj'] = q4_128_config
-        quant_config[f'model.layers.{i}.self_attn.v_proj'] = q4_128_config
-        quant_config[f'model.layers.{i}.self_attn.o_proj'] = q4_32_config
+        quant_config[f'model.layers.{i}.self_attn.q_proj'] = q4_256_config
+        quant_config[f'model.layers.{i}.self_attn.k_proj'] = q4_256_config
+        quant_config[f'model.layers.{i}.self_attn.v_proj'] = q4_256_config
+        quant_config[f'model.layers.{i}.self_attn.o_proj'] = q4_128_config
         
-        quant_config[f'model.layers.{i}.mlp.gate_proj'] = q4_128_config
-        quant_config[f'model.layers.{i}.mlp.up_proj'] = q4_128_config
-        quant_config[f'model.layers.{i}.mlp.down_proj'] = q4_32_config
+        quant_config[f'model.layers.{i}.mlp.gate_proj'] = q4_256_config
+        quant_config[f'model.layers.{i}.mlp.up_proj'] = q4_256_config
+        quant_config[f'model.layers.{i}.mlp.down_proj'] = q4_128_config
         
     return quant_config
